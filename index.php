fcc374c1b2cf098cfee19e30da86ea984bbd54a0
Rasmus Andersen <rasmus.skovhaven@gmail.com>
2021-05-10 12:46:31 +0200

    initial commit
    initial description

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// get header
+require_once "includes/header.php";
+
+$page = '';
+
+// get full page path
+$page = (isset($_GET['page'])) ? "pages/" . $_GET['page'] . '.php' : 'pages/forside.php';
+
+// if page doesnt exist, redirect to 404
+if (file_exists($page)) {
+    include_once($page);
+} else {
+    include_once 'pages/error404.php';
+}
+
+// get footer
+require_once 'includes/footer.php';
