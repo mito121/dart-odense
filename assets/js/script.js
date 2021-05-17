a1b2a2f1ecb2806d66b53eaac7729ca2484a0130
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2021-05-17 20:01:24 +0200

    stuff is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,25 @@ $(document).ready(function () {
   /*
    *** Slickjs
    */
-  $('.slick-slider').slick({
+  $(".slick-slider").slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     infinite: false,
-    arrows: true
-  })
+    arrows: true,
+  });
 
   /*
    *** Magical gallery
    */
-  const paths = ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG']
-  const images = document.querySelectorAll('.magical-gallery-item img')
-  const i = 0
-  const length = paths.length
-})
+  const paths = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG"];
+  const images = document.querySelectorAll(".magical-gallery-item img");
+
+  //window.setInterval(function () {
+  for (let i = 0; i < images.length; i++) {
+    window.setInterval(function () {
+      const newPath = paths[~~(Math.random() * paths.length)];
+      images[i].src = "assets/img/" + newPath;
+    }, 5000);
+  }
+  //}, 700);
+});
