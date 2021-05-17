fba8b26cd71406712b3abcce2019ac5c997a495d
Rasmus Andersen <rasmus.skovhaven@gmail.com>
2021-05-17 15:11:38 +0200

    lortet virker ikke

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-/* News slider */
-/*
- *** Slickjs
- */
 $(document).ready(function () {
+  /* News slider */
+  /*
+   *** Slickjs
+   */
   $('.slick-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     infinite: false,
     arrows: true
   })
+
+  /*
+   *** Magical gallery
+   */
+  const paths = ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG']
+  const images = document.querySelectorAll('.magical-gallery-item img')
+  const i = 0
+  const length = paths.length
 })
